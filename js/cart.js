19dc91264c667f6fe7f4e96d2cb51de44e20d273
Samar Panda <samar.panda84@gmail.com>
2012-08-13 08:43:22 +0530

    Added backbone.js and underscore.js. Basic app updated.

--- js/cart.js
+++ js/cart.js
@@ -7,8 +7,7 @@
 			Collection: {},
 			View: {}
 		};
-
-	//Base item Model
+	
 	App.Model.Item = Backbone.Model.extend({
 		defaults:{
 			photo: "images/temp.png",
@@ -21,26 +20,43 @@
 		model: App.Model.Item
 	});
 
-	var items = [
-	  { title: "Macbook Air", price: 799 },
-	  { title: "Macbook Pro", price: 999 },
-	  { title: "The new iPad", price: 399 },
-	  { title: "Apple Mouse", price: 50 },
-	  { title: "Dell Monior", price: 799 }
-	];
-
-	var cartCollection = new App.Collection.Cart(items);
-
 	App.View.ItemView = Backbone.View.extend({
 		className: "item-wrap",
 		template: $("#itemTemplate").html(),
 
 		render:function(){
 			//Render Item
+			var templ = _.template(this.template);
+			this.$el.html(templ(this.model.toJSON()));
 			return this;
 		}
 	});
 
+	App.View.CartCollectionView = Backbone.View.extend({
+		el:"#mycart",
+		initialize: function(){
+			//Temp data
+			var items = [
+			  { title: "Macbook Air", price: 799 },
+			  { title: "Macbook Pro", price: 999 },
+			  { title: "The new iPad", price: 399 },
+			  { title: "Apple Mouse", price: 50 },
+			  { title: "Dell Monior", price: 799 }
+			];
+			this.collection = new App.Collection.Cart(items);
+			this.render();
+		},
+		render: function(){
+			this.collection.each(function(item){
+				this.renderItem(item);
+			}, this);
+		},
+		renderItem: function(item){
+			var itemView = new App.View.ItemView({model: item});
+			this.$el.append(itemView.render().el);
+		}
+	});
+	
 	var sBASE = function(){
 		var UI      = {};
 		var EXT     = {};
@@ -53,7 +69,7 @@
 			'PROJECT': PROJECT
 		};
 	}();
-
-
+	
+	var cartObj = new App.View.CartCollectionView();
 
 })(window);
