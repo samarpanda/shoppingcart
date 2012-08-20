41e6567fa9a00dc2a157c26c5adc460352927516
Samar Panda <samar.panda84@gmail.com>
2012-08-21 00:37:45 +0530

    Rendering multiple views using backbonejs

---
+++ js/bipinview.js
@@ -0,0 +1,46 @@
+﻿(function(window, undefined){
+  var $ = jQuery,
+      App = {
+          Model: {},
+          Collection: {},
+          View: {}
+      };
+  App.Model.User = Backbone.Model.extend({});
+  App.Collection.OnlineUsers = Backbone.Collection.extend({
+      model: App.Model.User
+  });
+  App.View.CounterView = Backbone.View.extend({
+      tagName: "span",
+      initialize: function(){
+        _.bindAll(this, 'render');
+        this.collection.on('add', this.render);
+      },
+      render: function(){
+        this.$el.text(this.collection.size());
+        return this;
+      }
+  });
+  App.View.ListView = Backbone.View.extend({
+      initialize: function(){
+        _.bindAll(this, 'render');
+        this.collection.on('add', this.render);
+      },
+      render: function(){
+        var html = this.collection.map(function(m){
+            return '<p>'+m.get('name')+'</p>';
+        });
+        this.$el.html(html.join(''));
+        return this;
+      }
+  });
+    
+  var online = new App.Collection.OnlineUsers();
+  var counter = new App.View.CounterView({collection: online});
+  var list = new App.View.ListView({collection: online});
+  $('#counter').append(counter.render().el);
+  $('#main').append(list.render().el);
+    
+  $('#add').click(function(){
+      online.add({ name: 'User ' + (online.size() + 1)});
+  });
+})(window);
